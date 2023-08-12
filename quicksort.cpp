8b16e1deb0e5c1018bbd052b1179f826947d3a4d
yaswanth8978 <115204301+yaswanth8978@users.noreply.github.com>
2023-08-12 09:19:13 +0530

    Create quicksort.cpp

---
+++ quicksort.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <conio.h>
+void qsort(int arr[20], int fst, int last);
+
+int main()
+{
+	int arr[30];
+	int i,n;
+
+	printf("Enter total no. of the elements : "); 
+	scanf("%d", &n);
+
+	printf("Enter total %d elements : \n", size); 
+	for(i=0; i<n; i++)
+		scanf("%d", &arr[i]);
+	
+		qsort(arr,0,n-1);
+	
+	printf("\n Quick sorted elements \n");
+	for(i=0; i<n; i++)
+		printf("%d\t", arr[i]);
+}
+
+void qsort(int arr[20], int fst, int last)
+{
+	int i, j, pivot, tmp;
+	if(fst < last)
+	{
+		pivot = fst;
+		i = fst;
+		j = last;
+		while(i < j)
+		{
+			while(arr[i] <=arr[pivot] && i<last)
+				i++;
+			while(arr[j] > arr[pivot])
+				j--;
+			if(i <j )
+			{
+				tmp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = tmp;
+			}
+		}
+		tmp = arr[pivot];
+		arr[pivot] = arr[j];
+		arr[j] = tmp;
+		qsort(arr, fst, j-1);
+		qsort(arr, j+1, last);
+	}
+}
